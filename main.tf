4bd902c065f864997cc83b61a106754bec9261c0
anil <anil@gamil.com>
2024-09-02 20:20:15 +0530

    adding second rg in my code

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rgblock" {
   name = "Rg-Pardeep"
   location = "Central India"
 }
+
+resource "azurerm_resource_group" "rgblock" {
+  name = "Rg-Arati"
+  location = "Central India"
+}
